cbae11a918508cc36d04a65c814f449ec9855124
Jason Meridth <jmeridth@gmail.com>
2011-03-03 01:49:15 -0600

    add licensing to all files.  change build to use albacore gem. add bzrignore file

--- src/xenserver/windows/rakefile.rb
+++ src/xenserver/windows/rakefile.rb
@@ -1,82 +1,135 @@
-require 'fileutils'
-require 'BuildUtils'
-require "Properties"
-require 'zip/zip'
-require 'find'
-require 'rake/clean'
+require 'rubygems'
+require 'albacore'
+require "Properties.rb"
+require "fileutils"
 
-CLEAN.include(UNITTESTS_RESULTS_DIR, output_dir, "src/**/bin", "src/**/obj")
+desc "compiles, runs tests and creates zip file"
+task :all => [:default]
 
-task :default => ["clean", "build:all", "package", "finish"]
+desc "compiles, runs tests and creates zip file"
+task :default => [:compile, :tests, :package]
 
-namespace :build do
-  task :all => [:compile, :test]
+desc "Versioning"
+task :version do
+  puts "The build number is #{RELEASE_BUILD_NUMBER}"
+  Rake::Task["agent:assemblyinfo"].execute
+  Rake::Task["agent_service:assemblyinfo"].execute
+  Rake::Task["update_service:assemblyinfo"].execute
+  Rake::Task["common:assemblyinfo"].execute
+  Rake::Task["diffie_hellman:assemblyinfo"].execute
+end
 
-  desc "creates output directories"
-  task :create do
-    FileUtils.mkdir UNITTESTS_RESULTS_DIR unless File.exists?(UNITTESTS_RESULTS_DIR)
-    FileUtils.mkdir output_dir unless File.exists?(output_dir)
-  end
-
-  desc "version the assemblies"
-  task :version do
-    puts "writing AssemblyInfo files ..."
-    asmInfoBuilder = AssemblyInfoBuilder.new(SOURCE_DIR,   
-                                            {:version => RELEASE_VERSION,
-                                            :buildNumber => RELEASE_BUILD_NUMBER,
-                                            :product => PRODUCT, 
-                                            :copyright => COPYRIGHT,
-                                            :company => COMPANY,
-                                            :description => DESCRIPTION})
-    asmInfoBuilder.do
-  end
-
-  desc "code compile"
-  task :compile => [:create, :version] do
-	puts "BUILD REVISION: #{ENV['CC_BUILD_REVISION']}"
-	puts "BUILD LABEL: #{ENV['CC_BUILD_LABEL']}"
-	puts "BUILD ARTIFACTS: #{ENV['CC_BUILD_ARTIFACTS']}"
-    puts "compiling solution(s) ..."
-    solutions = FileList["#{SOURCE_DIR}/**/*.sln"]
-    solutions.each do |solution|
-      sh "#{MSBUILD_DIR}\\msbuild.exe /t:Clean;Rebuild /p:Configuration=Debug #{solution}"
-    end
-  end
-
-  desc "runs tests"
-  task :test => [:compile] do
-    puts "running unit tests ..."
-    tests = FileList["#{SOURCE_DIR}/**/*.Specs.dll"].exclude(/obj\//)
-    sh "#{NUNIT_EXE} #{tests} /nologo /xml=#{UNITTESTS_RESULTS_DIR}/TestResults.xml"
+namespace :agent do
+  desc "Update the version information for agent library"
+  assemblyinfo :assemblyinfo do |asm|
+    asm.version = RELEASE_BUILD_NUMBER
+    asm.company_name = COMPANY
+    asm.product_name = PRODUCT
+    asm.description = DESCRIPTION
+    asm.copyright = COPYRIGHT
+    asm.output_file = File.join(ABSOLUTE_PATH,'src','Rackspace.Cloud.Server.Agent','Properties','AssemblyInfo.cs')
   end
 end
 
-desc "create index.html file for custom artifacts"
-task :create_artifact_index do
-  INDEX_FILE_NAME = 'index.html'
+namespace :common do
+  desc "Update the version information for common library"
+  assemblyinfo :assemblyinfo do |asm|
+    asm.version = RELEASE_BUILD_NUMBER
+    asm.company_name = COMPANY
+    asm.product_name = PRODUCT
+    asm.description = DESCRIPTION
+    asm.copyright = COPYRIGHT
+    asm.output_file = File.join(ABSOLUTE_PATH,'src','Rackspace.Cloud.Server.Agent','Properties','AssemblyInfo.cs')
+  end
+end
+
+namespace :diffie_hellman do
+  desc "Update the version information for diffie hellman library"
+  assemblyinfo :assemblyinfo do |asm|
+    asm.version = RELEASE_BUILD_NUMBER
+    asm.company_name = COMPANY
+    asm.product_name = PRODUCT
+    asm.description = DESCRIPTION
+    asm.copyright = COPYRIGHT
+    asm.output_file = File.join(ABSOLUTE_PATH,'src','Rackspace.Cloud.Server.Agent','Properties','AssemblyInfo.cs')
+  end
+end
+
+desc "Compile the solution"
+msbuild :compile => :version do |msb|
+  msb.command = MSBUILD_EXE
+  msb.properties :configuration => COMPILE_TARGET
+  msb.targets :Rebuild
+  msb.verbosity = 'minimal'
+  msb.solution = SLN_FILE
+end
+
+desc "Run unit tests"
+task :tests => [:agent_specs, :diffiehellman_specs]
+
+desc "Run agent unit tests"
+nunit :agent_specs => :compile do |nunit|
+  nunit.command = NUNIT_CMD_EXE
+  nunit.assemblies AGENT_UNIT_TEST_DLL
+  nunit.options '/xml=agent-unit-tests-results.xml'
+end
+
+desc "Run diffiehellman unit tests"
+nunit :diffiehellman_specs => :compile do |nunit|
+  nunit.command = NUNIT_CMD_EXE
+  nunit.assemblies DIFFIEHELLMAN_UNIT_TEST_DLL
+  nunit.options '/xml=diffiehellman-unit-tests-results.xml'
+end
+
+desc "Packaging"
+task :package do
+  Dir.mkdir BUILDS_DIR if !File.directory?(BUILDS_DIR)
+  Rake::Task["agent_service:zip"].execute
+  Rake::Task["update_service:zip"].execute
+end
+
+namespace :agent_service do
+  desc "Update the version information for agent service library"
+  assemblyinfo :assemblyinfo do |asm|
+    asm.version = RELEASE_BUILD_NUMBER
+    asm.company_name = COMPANY
+    asm.product_name = PRODUCT
+    asm.description = DESCRIPTION
+    asm.copyright = COPYRIGHT
+    asm.output_file = File.join(ABSOLUTE_PATH,'src','Rackspace.Cloud.Server.Agent.Service','Properties','AssemblyInfo.cs')
+  end
   
-  INDEX = <<-EOS
-<html>
-<body>
-  <a href="#{output_dir}\\AgentService.zip">AgentService.zip</a><br/>
-  <a href="#{output_dir}\\UpdateService.zip">UpdateService.zip</a>
-</body>
-</html>
-  EOS
-
-  File.open("#{output_dir}\\#{INDEX_FILE_NAME}", 'w') {|f| f.write(INDEX) }
+  desc "Create zip for agent service"
+  zip do |zip|
+    file = 'AgentService.zip'
+    File.delete(file) if File.exists?(file)
+    zip.output_path = BUILDS_DIR
+    zip.directories_to_zip AGENT_SERVICE_DIR
+    zip.additional_files = "installagentservice.bat"
+    zip.output_file = file
+    puts "Agent Service zip file created"
+  end
 end
 
-desc "dumps release dll's into zip file(s)"
-task :package do 
-  puts "creating build packages ..."
-  zipper = ReleaseZipper.new
-  zipper.create("#{output_dir}\\AgentService.zip", AGENT_SERVICE_DIR, "installagentservice.bat")
-  zipper.create("#{output_dir}\\UpdateService.zip", UPDATE_SERVICE_DIR, "installupdateservice.bat")
-  Rake::Task["create_artifact_index"].execute
-end
-
-desc "performs any post complete functions"
-task :finish do 
-  puts "ALL DONE."
+namespace :update_service do
+  desc "Update the version information for udpate service library"
+  assemblyinfo :assemblyinfo do |asm|
+    asm.version = RELEASE_BUILD_NUMBER
+    asm.company_name = COMPANY
+    asm.product_name = PRODUCT
+    asm.description = DESCRIPTION
+    asm.copyright = COPYRIGHT
+    asm.output_file = File.join(ABSOLUTE_PATH,'src','Rackspace.Cloud.Server.Agent.UpdaterService','Properties','AssemblyInfo.cs')
+  end
+  
+  desc "Create zip for update service"
+  zip do |zip|
+    file = 'UpdateService.zip'
+    File.delete(file) if File.exists?(file)
+    zip.output_path = BUILDS_DIR
+    zip.directories_to_zip UPDATE_SERVICE_DIR
+    zip.additional_files = "installupdateservice.bat"
+    zip.output_file = file
+    puts "Update Service zip file created"
+  end
 end
