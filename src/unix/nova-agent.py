014f18e28b0b96185942250a5aef464b7228276f
Chris Behrens <cbehrens@codestud.com>
2011-04-13 12:55:10 -0700

    Updated unix agent to 0.0.1.3 Added README Documented nova-agent.py

--- src/unix/nova-agent.py
+++ src/unix/nova-agent.py
@@ -1,16 +1,23 @@
-
-import sys
+# Needed to register the exchange/parser plugin combiniation with the
+# main daemon
 import agentlib
+
+# To get jsonparser and xscomm
 import plugins
+
+# Loads 'commands' plus all modules that contain command classes
 import commands.command_list
 
-
+# Not required, as the default is False
 test_mode = False
 
-args = {"test": "test123"}
-c = commands.init(**args)
+# Inits all command classes
+c = commands.init()
 
+# Creates instance of JsonParser, passing in available commands
 parser = plugins.JsonParser(c)
+# Create the XSComm intance
 xs = plugins.XSComm()
 
+# Register an exchange/parser combination with the main daemon
 agentlib.register(xs, parser)
